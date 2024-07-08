7d215139bb40e4233e799b4fe40379b32642a3d4
abhixshake <abhisheks10901@gmail.com>
2024-07-08 16:38:01 +0530

    Tetsing

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hey There");
+//sample addition (var,let,const)
+let value1=8;
+let value2=2;
+let sum=value1+value2;
+console.log("Sum",sum);
+console.log(sum);
+console.log(value1,value2)
+console.log(value1+''+value2)
+
+//
+
